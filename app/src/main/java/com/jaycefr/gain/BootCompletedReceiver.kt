cc495d50c617b01061b7b6605abc75f14b6d446d
JayceFR <jaycejefferson.vicious@gmail.com>
2024-09-24 11:59:24 +0100

    autostart after reboot

--- app/src/main/java/com/jaycefr/gain/BootCompletedReceiver.kt
+++ app/src/main/java/com/jaycefr/gain/BootCompletedReceiver.kt
@@ -3,19 +3,17 @@ package com.jaycefr.gain
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import com.jaycefr.gain.steps.serivces.Actions
+import com.jaycefr.gain.steps.serivces.StepForegroundService
 
 class BootCompletedReceiver : BroadcastReceiver() {
     override fun onReceive(context: Context?, intent: Intent?) {
         if (intent!!.action == Intent.ACTION_BOOT_COMPLETED){
-            val i = Intent(context, MainActivity::class.java)
-            i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
-            context!!.startActivity(i)
-//            context?.startForegroundService(serviceIntent)
-//            GlobalScope.launch(Dispatchers.Default){
-//                if (context != null) {
-//                    StepWidget.updateAll(context)
-//                }
-//            }
+
+            val serviceIntent = Intent(context, StepForegroundService::class.java)
+            serviceIntent.action = Actions.START.toString()
+            context!!.startForegroundService(serviceIntent)
+
         }
     }
 
